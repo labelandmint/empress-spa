1f75c9cf4e66c55f9af289dec1f1b9f5cd99dd16
Sulayman <sulayman.mohmand@gmail.com>
2024-11-02 21:55:24 +1100

    migrations updated

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->id(); // Adds an auto-incrementing primary key
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();

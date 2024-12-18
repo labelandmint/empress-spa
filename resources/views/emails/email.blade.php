72d3acd533fa6f9c8c9280931545f1f158f76051
Nishant Beniwal <nishantbeniwal@Nishants-MacBook.local>
2024-12-18 17:26:00 +0530

    latest changes

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -4,7 +4,7 @@
     <title>Welcome to Our Service</title>
 </head>
 <body>
-    <h3 style="text-align: center;font-size: 24px;">Hello {{$name}}</h3>
+    <h3 style="text-align: center;font-size: 24px;">Hello {{ $name }}</h3>
     <div class="d-flex align-items-center justify-content-between es-text-lg es-font-mulish-bold">
         <!-- <div class="d-flex justify-content-end">
             <button type="button" class="border-0 bg-transparent" data-bs-dismiss="modal">
@@ -12,17 +12,29 @@
             </button>
         </div> -->
     </div>
-    <div class=""  style="text-align: center;padding: 100px 0px;" >
-        <img class="img600x100 img-fluid" src="{{url('images/'.$logo)}}" alt=""/>
+    <div class="" style="text-align: center;
+               padding: 100px 0px;
+             ">
+        <img class="img600x100 img-fluid"
+            style="width: 600px;
+                   height: 100px;
+                   object-fit: cover;
+                   object-position: center;"
+            src="{{ url('images/' . $logo) }}" alt="" />
     </div>
     <div class="" style="text-align: center;">
-        <img class="img600x300 img-fluid" src="{{url('images/'.$graphic)}}" alt="" />
+        <img class="img600x300 img-fluid"
+            style="  width: 600px;
+                      height: 300px;
+                      object-fit: cover;
+                      object-position: center;"
+            src="{{ url('images/' . $graphic) }}" alt="" />
     </div>
     <div class="" style="text-align: center; font-size : 24px;">
-        <h3>{{$header}}</h3>
+        <h3>{{ $header }}</h3>
     </div>
     <div class="" style="text-align: center ; font-size:18px;margin-bottom: 20px;">
-        {{$body}}
+        {{ $body }}
     </div>
 </body>
 </html>
